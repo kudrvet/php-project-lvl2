e620659bc436d9f093d4f208f648a97b240daddf
Vitaliy <kudrvet@gmail.com>
2020-07-23 17:15:26 +0300

    lint Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -2,41 +2,37 @@
 
 namespace Differ\Differ;
 
-function genDiff ($path1, $path2)
+function genDiff($path1, $path2)
 {
-        $file1Content = file_get_contents($path1, true);
-        $file2Content = file_get_contents($path2, true);
+    $file1Content = file_get_contents($path1, true);
+    $file2Content = file_get_contents($path2, true);
+    $beforeAsoc = json_decode($file1Content, true);
+    $afterAsoc = json_decode($file2Content, true);
+    $result = "";
+    foreach ($afterAsoc as $key => $value) {
+        if (is_bool($value)) {
+            $value = ($value) ? 'true' : 'false';
+        }
 
-        $beforeAsoc = json_decode($file1Content, true);
-        $afterAsoc = json_decode($file2Content, true);
-
-        $result ="";
-        foreach ($afterAsoc as $key => $value)
-        {
-            if (is_bool($value)) {
-                $value = ($value) ? 'true' :'false';
-            }
-
-            if(isset($beforeAsoc[$key])) {
-                // not changed
-                if ($beforeAsoc[$key] == $value) {
-                    $result = $result."{$key} :{$value} \n";
-                    // changed
-                } elseif ($beforeAsoc[$key] !== $value) {
-                    $result .= "+ {$key}: {$value} \n";
-                    $result .= "- {$key}: {$beforeAsoc[$key]} \n";
-                }
-                //add
-            } else {
+        if (isset($beforeAsoc[$key])) {
+            // not changed
+            if ($beforeAsoc[$key] == $value) {
+                $result = $result . "{$key} :{$value} \n";
+                // changed
+            } elseif ($beforeAsoc[$key] !== $value) {
                 $result .= "+ {$key}: {$value} \n";
+                $result .= "- {$key}: {$beforeAsoc[$key]} \n";
             }
+            //add
+        } else {
+            $result .= "+ {$key}: {$value} \n";
         }
+    }
         //deleted
-        $deleted = array_diff_key($beforeAsoc, $afterAsoc);
-        foreach ($deleted as $key => $value) {
-            $result .= "- {$key}: {$value} \n";
-        }
-
+    $deleted = array_diff_key($beforeAsoc, $afterAsoc);
+    foreach ($deleted as $key => $value) {
+        $result .= "- {$key}: {$value} \n";
+    }
         return "{\n$result}" ;
 }
 
