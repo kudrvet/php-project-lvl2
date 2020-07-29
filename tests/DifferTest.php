0c91c80bfa7e4843d06bf5f46a49fe4c3e1a8642
Vitaliy <kudrvet@gmail.com>
2020-07-29 17:00:15 +0300

    update DifferTest

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -11,19 +11,19 @@ if (file_exists($autoloadPath1)) {
 }
 
 use PHPUnit\Framework\TestCase;
-use \Differ\Differ\genDiff;
+use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
     public function testGenDiffWithEqualsStrings()
     {
-        $diff = $diff = \Differ\Differ\genDiff('./tests/fixtures/before1.json','./tests/fixtures/after1.json');
+        $diff = $diff = genDiff('./tests/fixtures/before1.json','./tests/fixtures/after1.json');
         $this->assertEquals(file_get_contents('./tests/fixtures/trueResult1'),  $diff);
 
     }
     public function testGenDiff()
     {
-        $diff = \Differ\Differ\genDiff('./tests/fixtures/before.json','./tests/fixtures/after.json');
+        $diff = genDiff('./tests/fixtures/before.json','./tests/fixtures/after.json');
         $this->assertEquals(file_get_contents('./tests/fixtures/trueResult'),  $diff);
     }
 }
