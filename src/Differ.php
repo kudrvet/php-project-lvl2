8e679b307a995b84d994e96c79d4581b747edb85
Vitaliy <kudrvet@gmail.com>
2020-08-24 19:27:29 +0300

    update src/Differ.php, src/YamlPArser.php, tests/DifferTest and  some fixtures

--- src/Differ.php
+++ src/Differ.php
@@ -4,9 +4,19 @@ namespace Differ\Differ;
 
 use function Differ\Parsers\YamlParser\toAsoc;
 use function  Differ\Parsers\JsonParser\toAsoc as jsonToAsoc;
+use function Funct\Strings\strip;
+
+function boolToString($value)
+{
+    if (is_bool($value)) {
+        return ($value) ? 'true' : 'false';
+    }
+    return $value;
+}
 
 function genDiff($path1, $path2)
 {
+
     $fileBefore = file_get_contents($path1, true);
     $fileAfter = file_get_contents($path2, true);
 
@@ -19,11 +29,21 @@ function genDiff($path1, $path2)
             break;
         case "json":
             $beforeAsoc = jsonToAsoc($fileBefore);
-            $afterAsoc =  jsonToAsoc($fileAfter);
+            $afterAsoc = jsonToAsoc($fileAfter);
             break;
     }
 
-    $result = "";
+    $ast = getDiffAST($beforeAsoc, $afterAsoc);
+    return toFormat($ast);
+}
+
+function getDiffAST(array $beforeAsoc, array $afterAsoc)
+{
+    $diff = [];
+
+    foreach ($beforeAsoc as $key => $value) {
+        $value = boolToString($value);
+    }
     foreach ($afterAsoc as $key => $value) {
         if (is_bool($value)) {
             $value = ($value) ? 'true' : 'false';
@@ -31,22 +51,92 @@ function genDiff($path1, $path2)
 
         if (isset($beforeAsoc[$key])) {
             // not changed
-            if ($beforeAsoc[$key] == $value) {
-                $result = $result . "{$key}: {$value}\n";
+            if ($beforeAsoc[$key] === $value) {
+                $diff[] = ['key' => $key, 'value' => $value, 'status' => 'unchanged'];
                 // changed
             } elseif ($beforeAsoc[$key] !== $value) {
-                $result .= "+ {$key}: {$value}\n";
-                $result .= "- {$key}: {$beforeAsoc[$key]}\n";
+                if (is_array($beforeAsoc[$key]) && is_array($afterAsoc[$key])) {
+                    $childrenBefore = $beforeAsoc[$key];
+                    $childrenAfter = $afterAsoc[$key];
+                    $diff [] = ['key' => $key, 'status' => 'nested',
+                        'children' => getDiffAST($childrenBefore, $childrenAfter)];
+                } else {
+                    $diff[] = ['key' => $key, 'oldValue' => boolToString($beforeAsoc[$key]),
+                        'newValue' => $value, 'status' => 'changed'];
+                }
             }
             //add
         } else {
-            $result .= "+ {$key}: {$value}\n";
+            $diff[] = ['key' => $key, 'value' => $value, 'status' => 'added'];
         }
     }
         //deleted
-    $deleted = array_diff_key($beforeAsoc, $afterAsoc);
+        $deleted = array_diff_key($beforeAsoc, $afterAsoc);
     foreach ($deleted as $key => $value) {
-        $result .= "- {$key}: {$value}\n";
+        $diff[] = ['key' => $key, 'value' => boolToString($value), 'status' => 'deleted'];
     }
-        return "{\n$result}" ;
+    return $diff ;
+}
+
+function toFormat($diffAST)
+{
+    return "{\n" . toFormatMain($diffAST, $deep = 0) . "}";
+}
+function toFormatMain($diffAST, $deep)
+{
+    $formatMap = ['added' => '+ ','deleted' => '- ','unchanged' => '  '];
+    $res = "";
+    foreach ($diffAST as $key => $value) {
+        $status = $diffAST[$key]['status'];
+        $keyIn = $diffAST[$key]['key'];
+        $tab = str_repeat("    ", $deep);
+        $res .= $tab;
+        if ($status == 'nested') {
+            $children = $diffAST[$key]['children'];
+            $res = $res . "    " . $keyIn . ": {\n"
+                . toFormatMain($children, $deep + 1)
+                . str_repeat("    ", $deep + 1) . "}\n";
+        } else {
+            $res .= "  ";
+
+            if ($status == 'changed') {
+                if (is_array($diffAST[$key]['oldValue'])) {
+                    $res = $res . '- ' . $keyIn . ": {"
+                        . formatArray($diffAST[$key]['oldValue'], $deep)
+                        . str_repeat("    ", $deep + 1) . "}\n";
+                } else {
+                    $res = $res . '- ' . $keyIn . ': ' . $diffAST[$key]['oldValue'] . "\n";
+                }
+                // в случае changed нужно добавить еще "  " для смещения newValue;
+                $res .= $tab . "  ";
+                if (is_array($diffAST[$key]['newValue'])) {
+                    $res = $res . '+ ' . $keyIn . ": {"
+                        . formatArray($diffAST[$key]['newValue'], $deep)
+                        . str_repeat("    ", $deep + 1) . "}\n";
+                } else {
+                    $res = $res . '+ ' . $keyIn . ': ' . $diffAST[$key]['newValue'] . "\n";
+                }
+            } else {
+                if (is_array($diffAST[$key]['value'])) {
+                    $res = $res . $formatMap[$status] . $keyIn . ": {"
+                        . formatArray($diffAST[$key]['value'], $deep)
+                        . str_repeat("    ", $deep + 1) . "}\n";
+                } else {
+                    $res = $res . $formatMap[$status] . $keyIn . ': ' . $diffAST[$key]['value'] . "\n";
+                }
+            }
+        }
+    }
+
+    return $res;
+}
+
+function formatArray($array, $deep)
+{
+    $stripedStr = strip(json_encode($array, JSON_PRETTY_PRINT), "\"", ",");
+    $trimed = trim($stripedStr, "{} ");
+    $tab = str_repeat("    ", $deep + 1);
+    $res = str_replace("\n    ", "\n" . $tab . "    ", $trimed);
+
+    return $res;
 }
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -15,28 +15,40 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    public function testGenDiffWithEqualsJsonStrings()
-    {
-        $diff = $diff = genDiff('./tests/fixtures/jsonBefore1.json','./tests/fixtures/jsonAfter1.json');
-        $this->assertEquals(file_get_contents('./tests/fixtures/trueResult1'),  $diff);
 
-    }
     public function testGenDiffWithJson()
     {
+
         $diff = genDiff('./tests/fixtures/jsonBefore2.json','./tests/fixtures/jsonAfter2.json');
         $this->assertEquals(file_get_contents('./tests/fixtures/trueResult2'),  $diff);
-    }
-
-    public function testGenDiffWithEqualsYamlStrings()
-    {
-        $diff = $diff = genDiff('./tests/fixtures/ymlBefore1.yaml', './tests/fixtures/ymlAfter1.yaml');
-        $this->assertEquals(file_get_contents('./tests/fixtures/trueResult1'), $diff);
 
     }
+
     public function testGenDiffWithYaml()
     {
         $diff = genDiff('./tests/fixtures/ymlBefore2.yaml','./tests/fixtures/ymlAfter2.yaml');
         $this->assertEquals(file_get_contents('./tests/fixtures/trueResult2'),  $diff);
     }
+
+      public function testGenDiffWithRecursiveJson ()
+      {
+          $diff = genDiff('./tests/fixtures/recursiveJsonBefore.json',
+              './tests/fixtures/recursiveJsonAfter.json');
+          $expected = file_get_contents('./tests/fixtures/trueResultRecursive');
+          $this->assertEquals($expected,$diff);
+      }
+
+    public function testGenDiffWithRecursiveYml ()
+    {
+        $diff = genDiff('./tests/fixtures/recursiveYmlBefore.yaml',
+            './tests/fixtures/recursiveYmlAfter.yaml');
+        $expected = file_get_contents('./tests/fixtures/trueResultRecursiveYml');
+        $this->assertEquals($expected,$diff);
+    }
+
 }
+
+
+
+
 
