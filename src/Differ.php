fabf07df2e00e501dbf1f7fd010ea3697d58aca1
Vitaliy <kudrvet@gmail.com>
2020-08-26 14:35:03 +0300

    add PlainFormatter, refactor

--- src/Differ.php
+++ src/Differ.php
@@ -2,8 +2,10 @@
 
 namespace Differ\Differ;
 
+use function Differ\Formatters\PrettyFormatter\toPrettyFormat;
+use function Differ\Formatters\PlainFormatter\toPlainFormat;
 use function Differ\Parsers\YamlParser\toAsoc;
-use function  Differ\Parsers\JsonParser\toAsoc as jsonToAsoc;
+use function Differ\Parsers\JsonParser\toAsoc as jsonToAsoc;
 use function Funct\Strings\strip;
 
 function boolToString($value)
@@ -14,7 +16,7 @@ function boolToString($value)
     return $value;
 }
 
-function genDiff($path1, $path2)
+function genDiff($path1, $path2, $format = 'pretty')
 {
 
     $fileBefore = file_get_contents($path1, true);
@@ -34,7 +36,13 @@ function genDiff($path1, $path2)
     }
 
     $ast = getDiffAST($beforeAsoc, $afterAsoc);
-    return toFormat($ast);
+
+    switch ($format) {
+        case 'pretty':
+            return toPrettyFormat($ast);
+        case 'plain':
+            return toPlainFormat($ast);
+    }
 }
 
 function getDiffAST(array $beforeAsoc, array $afterAsoc)
@@ -76,67 +84,4 @@ function getDiffAST(array $beforeAsoc, array $afterAsoc)
         $diff[] = ['key' => $key, 'value' => boolToString($value), 'status' => 'deleted'];
     }
     return $diff ;
-}
-
-function toFormat($diffAST)
-{
-    return "{\n" . toFormatMain($diffAST, $deep = 0) . "}";
-}
-function toFormatMain($diffAST, $deep)
-{
-    $formatMap = ['added' => '+ ','deleted' => '- ','unchanged' => '  '];
-    $res = "";
-    foreach ($diffAST as $key => $value) {
-        $status = $diffAST[$key]['status'];
-        $keyIn = $diffAST[$key]['key'];
-        $tab = str_repeat("    ", $deep);
-        $res .= $tab;
-        if ($status == 'nested') {
-            $children = $diffAST[$key]['children'];
-            $res = $res . "    " . $keyIn . ": {\n"
-                . toFormatMain($children, $deep + 1)
-                . str_repeat("    ", $deep + 1) . "}\n";
-        } else {
-            $res .= "  ";
-
-            if ($status == 'changed') {
-                if (is_array($diffAST[$key]['oldValue'])) {
-                    $res = $res . '- ' . $keyIn . ": {"
-                        . formatArray($diffAST[$key]['oldValue'], $deep)
-                        . str_repeat("    ", $deep + 1) . "}\n";
-                } else {
-                    $res = $res . '- ' . $keyIn . ': ' . $diffAST[$key]['oldValue'] . "\n";
-                }
-                // в случае changed нужно добавить еще "  " для смещения newValue;
-                $res .= $tab . "  ";
-                if (is_array($diffAST[$key]['newValue'])) {
-                    $res = $res . '+ ' . $keyIn . ": {"
-                        . formatArray($diffAST[$key]['newValue'], $deep)
-                        . str_repeat("    ", $deep + 1) . "}\n";
-                } else {
-                    $res = $res . '+ ' . $keyIn . ': ' . $diffAST[$key]['newValue'] . "\n";
-                }
-            } else {
-                if (is_array($diffAST[$key]['value'])) {
-                    $res = $res . $formatMap[$status] . $keyIn . ": {"
-                        . formatArray($diffAST[$key]['value'], $deep)
-                        . str_repeat("    ", $deep + 1) . "}\n";
-                } else {
-                    $res = $res . $formatMap[$status] . $keyIn . ': ' . $diffAST[$key]['value'] . "\n";
-                }
-            }
-        }
-    }
-
-    return $res;
-}
-
-function formatArray($array, $deep)
-{
-    $stripedStr = strip(json_encode($array, JSON_PRETTY_PRINT), "\"", ",");
-    $trimed = trim($stripedStr, "{} ");
-    $tab = str_repeat("    ", $deep + 1);
-    $res = str_replace("\n    ", "\n" . $tab . "    ", $trimed);
-
-    return $res;
 }
--- src/Formatters/PlainFormatter.php
+++ src/Formatters/PlainFormatter.php
@@ -1,1 +1,60 @@
 <?php
+
+namespace Differ\Formatters\PlainFormatter;
+
+use function Differ\Differ\boolToString;
+use function PHPUnit\Framework\isEmpty;
+
+function toPlainFormat($ast, $keysAncestors = "")
+{
+
+    return   array_reduce($ast, function ($output, $item) use ($keysAncestors) {
+        $status = $item['status'];
+        if ($status == 'nested') {
+            $children = $item['children'];
+            $keysAncestors .= empty($keysAncestors) ? "{$item['key']}" : ".{$item['key']}";
+            $output .= toPlainFormat($children, $keysAncestors);
+            return $output;
+        } else {
+            $output .= printProperty($item, $keysAncestors);
+            return  $output;
+        }
+    }, "");
+}
+
+
+function printProperty($item, $keysAncestors)
+{
+    $status = $item['status'];
+    $key = $item['key'];
+    $fullKeysPath = empty($keysAncestors) ? $key : $keysAncestors . "." . $key;
+    if ($status == 'unchanged') {
+        return  "";
+    }
+    if ($status == 'changed') {
+        $oldValue = is_array($item['oldValue']) ? '[complex value]' : $item['oldValue'];
+        $newValue = is_array($item['newValue']) ? '[complex value]' : $item['newValue'];
+
+        $oldValue = wrappingStrToQuotes($oldValue);
+        $newValue = wrappingStrToQuotes($newValue);
+
+        $res =  "Property '$fullKeysPath' was updated. From $oldValue to $newValue\n";
+    } else {
+        $value = $item['value'];
+        if ($status == 'deleted') {
+            $res = "Property '$fullKeysPath' was removed\n";
+        }
+
+        if ($status == 'added') {
+            $value = is_array($value) ? '[complex value]' : $value;
+            $value = wrappingStrToQuotes($value);
+            $res = "Property '$fullKeysPath' was added with value: {$value}\n";
+        }
+    }
+    return  $res;
+}
+
+function wrappingStrToQuotes($str)
+{
+    return ($str !== 'true' && $str !== 'false' && $str !== '[complex value]') ? "'" . $str . "'" : $str;
+}
