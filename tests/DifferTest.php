94e60e5ecb4d8930c48d2a0cf20c7f06c9206b9d
Vitaliy <kudrvet@gmail.com>
2020-09-25 18:03:54 +0300

    refactor tests/DifferTest

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -22,53 +22,27 @@ class DifferTest extends TestCase
      * @dataProvider additionProvider
      */
 
-    public function testGenDiff($pathsToCompareFiles,$formatterName, $pathToResultFile)
-
+    public function testGenDiff($pathToFileBefore,$pathToFileAfter,$formatterName, $pathToResultFile)
     {
-        [$pathToFileBefore, $pathToFileAfter] = $pathsToCompareFiles;
+
         $actual = genDiff($pathToFileBefore, $pathToFileAfter,$formatterName);
         $expected = file_get_contents($pathToResultFile);
 
         $this->assertEquals($expected, $actual);
-
     }
 
     public function additionProvider()
-
     {
         $res = [];
+        $pathToFixtureFolder = './tests/fixtures';
         foreach ($this->dataFormats as $dataFormat){
             foreach ($this->formatterNames as $formatterName) {
-                $res[] = [$this->getPathToCompareFiles($dataFormat),$formatterName,$this->getPathToResultFile($formatterName)];
+                $res[] = ["{$pathToFixtureFolder}/before.{$dataFormat}","{$pathToFixtureFolder}/after.{$dataFormat}",
+                    $formatterName,"{$pathToFixtureFolder}/{$formatterName}FormatterResult"];
             }
         }
         return $res;
     }
-
-    function getPathToCompareFiles($dataFormat)
-       {
-           switch ($dataFormat) {
-               case 'json':
-                   return ['./tests/fixtures/before.json','./tests/fixtures/after.json'];
-               case 'yaml':
-                   return ['./tests/fixtures/before.yaml','./tests/fixtures/after.yaml'];
-               default:
-                   throw new \Exception("Format {$dataFormat} is not supported");
-           }
-       }
-   function getPathToResultFile($formatterName)
-   {
-       switch ($formatterName) {
-           case 'pretty':
-               return './tests/fixtures/prettyFormatterResult';
-           case 'plain':
-               return './tests/fixtures/plainFormatterResult';
-           case 'json':
-               return './tests/fixtures/jsonFormatterResult.json';
-           default:
-               throw new \Exception("Formatter {$formatterName} is not supported");
-       }
-   }
 }
 
 
