54a5d26959ece2e679f7b8997cacee0dcb4d521a
Vitaliy <kudrvet@gmail.com>
2020-09-29 12:42:40 +0300

    refactor tests/DifferTest

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -2,15 +2,8 @@
 
 namespace Differ\Differ\Tests;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use PHPUnit\Framework\TestCase;
+
 use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
@@ -22,32 +15,29 @@ class DifferTest extends TestCase
      * @dataProvider additionProvider
      */
 
-    public function testGenDiff($pathToFileBefore,$pathToFileAfter,$formatterName, $pathToResultFile)
+    public function testGenDiff($dataFormat, $formatterName)
     {
-
-        $actual = genDiff($pathToFileBefore, $pathToFileAfter,$formatterName);
+        [$pathToFileBefore,$pathToFileAfter,$pathToResultFile] = $this->getFixturesPaths($dataFormat, $formatterName);
+        $actual = genDiff($pathToFileBefore, $pathToFileAfter, $formatterName);
         $expected = file_get_contents($pathToResultFile);
-
         $this->assertEquals($expected, $actual);
+    }
+
+    public function getFixturesPaths($dataFormat, $formatterName)
+    {
+        $pathToFixtureFolder = './tests/fixtures';
+        return ["{$pathToFixtureFolder}/before.{$dataFormat}","{$pathToFixtureFolder}/after.{$dataFormat}",
+                    "{$pathToFixtureFolder}/{$formatterName}FormatterResult"];
     }
 
     public function additionProvider()
     {
         $res = [];
-        $pathToFixtureFolder = './tests/fixtures';
-        foreach ($this->dataFormats as $dataFormat){
+        foreach ($this->dataFormats as $dataFormat) {
             foreach ($this->formatterNames as $formatterName) {
-                $res[] = ["{$pathToFixtureFolder}/before.{$dataFormat}","{$pathToFixtureFolder}/after.{$dataFormat}",
-                    $formatterName,"{$pathToFixtureFolder}/{$formatterName}FormatterResult"];
+                $res[] = [$dataFormat, $formatterName];
             }
         }
         return $res;
     }
 }
-
-
-
-
-
-
-
